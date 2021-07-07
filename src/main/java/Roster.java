f9ebdfc27b27f7196768a13ffe5733829be3f9f3
DreamCastl <DreamCastl@yandex.ru>
2021-07-07 04:00:54 +0300

    message

---
+++ src/main/java/Roster.java
@@ -0,0 +1,18 @@
+
+
+public class Roster {
+    public String NameRoster;
+    public String Faction ;
+    public int PowerLevel ;
+    public int RP ;
+
+
+
+    Roster(String NameRoster,String Faction, int PowerLevel ,int RP){
+        this.NameRoster = NameRoster;
+        this.Faction = Faction;
+        this.PowerLevel = PowerLevel;
+        this.RP = RP;
+
+    }
+}
