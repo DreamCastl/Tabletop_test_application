4c8f666a9a20ee51d1e0d8b6b8f7e8e65972a42c
DreamCastl <DreamCastl@yandex.ru>
2021-08-01 03:12:53 +0300

    разобрались с тегами

--- src/main/java/Roster.java
+++ src/main/java/Roster.java
@@ -1,3 +1,7 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.util.ArrayList;
 
 public class Roster {
@@ -9,8 +13,22 @@ public class Roster {
     public ArrayList<Unit> Units = new ArrayList<>();
 
     public String CreateHTMLView() {
-        String Result = "";
-        this.HTMlView = Result;
+
+        Document doc = Jsoup.parse("<html></html>");
+
+        doc.body().addClass("TableToRoster");
+
+        doc.body().appendElement("div");
+
+        doc.head().appendElement("style");
+
+        Element style = doc.select("style").first();
+        style.append("div powerlevel {font-size: 25px;}");
+
+        Element div = doc.select("div").first();
+        div.append("<PowerLevel>" +"Power Level:" + PowerLevel + "</PowerLevel>");
+
+        this.HTMlView = doc.toString();
     return this.HTMlView;}
 
 
